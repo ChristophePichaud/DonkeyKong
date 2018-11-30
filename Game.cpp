583372ae0b19571304ea34d237fbcf4959bd075d
Christophe Pichaud <christophep@cpixxi.com>
2018-11-30 21:15:57 +0100

    Add project files.

---
+++ Game.cpp
@@ -0,0 +1,169 @@
+#include "pch.h"
+#include "StringHelpers.h"
+#include "Game.h"
+#include "EntityManager.h"
+
+const float Game::PlayerSpeed = 100.f;
+const sf::Time Game::TimePerFrame = sf::seconds(1.f / 60.f);
+
+Game::Game()
+	: mWindow(sf::VideoMode(840, 600), "Donkey Kong 1981", sf::Style::Close)
+	, mTexture()
+	, mPlayer()
+	, mFont()
+	, mStatisticsText()
+	, mStatisticsUpdateTime()
+	, mStatisticsNumFrames(0)
+	, mIsMovingUp(false)
+	, mIsMovingDown(false)
+	, mIsMovingRight(false)
+	, mIsMovingLeft(false)
+{
+	mWindow.setFramerateLimit(160);
+
+	mTexture.loadFromFile("Media/Textures/Mario_small.png");
+
+	mPlayer.setTexture(mTexture);
+	mPlayer.setPosition(100.f, 500.f);
+
+	std::shared_ptr<Entity> player = std::make_shared<Entity>();
+	player->m_sprite = mPlayer;
+	player->m_type = EntityType::player;
+	player->m_size = mTexture.getSize();
+	player->m_position = mPlayer.getPosition();
+	EntityManager::m_Entities.push_back(player);
+
+	mStatisticsText.setFont(mFont);
+	mStatisticsText.setPosition(5.f, 5.f);
+	mStatisticsText.setCharacterSize(10);
+}
+
+void Game::run()
+{
+	sf::Clock clock;
+	sf::Time timeSinceLastUpdate = sf::Time::Zero;
+	while (mWindow.isOpen())
+	{
+		sf::Time elapsedTime = clock.restart();
+		timeSinceLastUpdate += elapsedTime;
+		while (timeSinceLastUpdate > TimePerFrame)
+		{
+			timeSinceLastUpdate -= TimePerFrame;
+
+			processEvents();
+			update(TimePerFrame);
+		}
+
+		updateStatistics(elapsedTime);
+		render();
+	}
+}
+
+void Game::processEvents()
+{
+	sf::Event event;
+	while (mWindow.pollEvent(event))
+	{
+		switch (event.type)
+		{
+		case sf::Event::KeyPressed:
+			handlePlayerInput(event.key.code, true);
+			break;
+
+		case sf::Event::KeyReleased:
+			handlePlayerInput(event.key.code, false);
+			break;
+
+		case sf::Event::Closed:
+			mWindow.close();
+			break;
+		}
+	}
+}
+
+void Game::update(sf::Time elapsedTime)
+{
+	sf::Vector2f movement(0.f, 0.f);
+	if (mIsMovingUp)
+		movement.y -= PlayerSpeed;
+	if (mIsMovingDown)
+		movement.y += PlayerSpeed;
+	if (mIsMovingLeft)
+		movement.x -= PlayerSpeed;
+	if (mIsMovingRight)
+		movement.x += PlayerSpeed;
+
+	for (std::shared_ptr<Entity> entity : EntityManager::m_Entities)
+	{
+		if (entity->m_enabled == false)
+		{
+			continue;
+		}
+
+		if (entity->m_type != EntityType::player)
+		{
+			continue;
+		}
+
+		entity->m_sprite.move(movement * elapsedTime.asSeconds());
+	}
+}
+
+void Game::render()
+{
+	mWindow.clear();
+
+	for (std::shared_ptr<Entity> entity : EntityManager::m_Entities)
+	{
+		if (entity->m_enabled == false)
+		{
+			continue;
+		}
+
+		mWindow.draw(entity->m_sprite);
+	}
+
+	mWindow.draw(mStatisticsText);
+	mWindow.display();
+}
+
+void Game::updateStatistics(sf::Time elapsedTime)
+{
+	mStatisticsUpdateTime += elapsedTime;
+	mStatisticsNumFrames += 1;
+
+	if (mStatisticsUpdateTime >= sf::seconds(1.0f))
+	{
+		mStatisticsText.setString(
+			"Frames / Second = " + toString(mStatisticsNumFrames) + "\n" +
+			"Time / Update = " + toString(mStatisticsUpdateTime.asMicroseconds() / mStatisticsNumFrames) + "us");
+
+		mStatisticsUpdateTime -= sf::seconds(1.0f);
+		mStatisticsNumFrames = 0;
+	}
+
+	//
+	// Handle collision
+	//
+
+	if (mStatisticsUpdateTime >= sf::seconds(0.050f))
+	{
+		// Handle collision weapon enemies
+	}
+}
+
+void Game::handlePlayerInput(sf::Keyboard::Key key, bool isPressed)
+{
+	if (key == sf::Keyboard::Up)
+		mIsMovingUp = isPressed;
+	else if (key == sf::Keyboard::Down)
+		mIsMovingDown = isPressed;
+	else if (key == sf::Keyboard::Left)
+		mIsMovingLeft = isPressed;
+	else if (key == sf::Keyboard::Right)
+		mIsMovingRight = isPressed;
+
+	if (key == sf::Keyboard::Space)
+	{
+	}
+}
