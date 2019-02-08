5336b7803e16360e5ccb9fc0147bd04651f7c00d
dormoy <dormoy.guillaume@gmail.com>
2019-02-08 23:11:36 +0100

    add space

--- src/Game.cpp
+++ src/Game.cpp
@@ -62,10 +62,10 @@ void Game::drawBlocks() {
         for (int j = 0; j < BLOCK_COUNT_Y; j++) {
             _Block[i][j].setTexture(_TextureBlock);
 
-            if (j%  2) {
-                _Block[i][j].setPosition(30.f + 70.f * (i + 1), 0.f + BLOCK_SPACE * (j + 1));
+            if (j %  2) {
+                _Block[i][j].setPosition(-20.f + 70.f * (i + 1), 0.f + BLOCK_SPACE * (j + 1));
             } else {
-                _Block[i][j].setPosition(90.f + 70.f * (i + 1), 0.f + BLOCK_SPACE * (j + 1));
+                _Block[i][j].setPosition(70.f + 70.f * (i + 1), 0.f + BLOCK_SPACE * (j + 1));
             }
 
             std::shared_ptr<Entity> se = std::make_shared<Entity>();
@@ -88,7 +88,7 @@ void Game::drawLadders() {
             _Ladder[i].setPosition(750.f + 70.f, 0.f + BLOCK_SPACE * (i + 1) + _sizeBlock.y);
         }
         else {
-            _Ladder[i].setPosition(120.f + 70.f, 0.f + BLOCK_SPACE * (i + 1) + _sizeBlock.y);
+            _Ladder[i].setPosition(100.f + 70.f, 0.f + BLOCK_SPACE * (i + 1) + _sizeBlock.y);
         }
 
 
